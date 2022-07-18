35fce4064ed9336256a7f378a5ab8edf86a9feb6
Fedomn <fedomn.ma@gmail.com>
2022-07-18 22:08:35 +0800

    feat(storage): add in-memory storage
    Signed-off-by: Fedomn <fedomn.ma@gmail.com>

--- src/storage/mod.rs
+++ src/storage/mod.rs
@@ -1,23 +1,28 @@
 mod csv;
+mod memory;
 use std::io;
 use std::sync::Arc;
 
 use arrow::error::ArrowError;
 use arrow::record_batch::RecordBatch;
 pub use csv::*;
+pub use memory::*;
 
 use crate::catalog::RootCatalog;
 
 #[derive(Clone)]
 pub enum StorageImpl {
     CsvStorage(Arc<CsvStorage>),
+    #[allow(dead_code)]
+    InMemoryStorage(Arc<InMemoryStorage>),
 }
 
 pub trait Storage: Sync + Send + 'static {
     type TableType: Table;
 
-    // currently only support create table by file
-    fn create_table(&self, id: String, filepath: String) -> Result<(), StorageError>;
+    fn create_csv_table(&self, id: String, filepath: String) -> Result<(), StorageError>;
+
+    fn create_mem_table(&self, id: String, data: Vec<RecordBatch>) -> Result<(), StorageError>;
 
     fn get_table(&self, id: String) -> Result<Self::TableType, StorageError>;
 
