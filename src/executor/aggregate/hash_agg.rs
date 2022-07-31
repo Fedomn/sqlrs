530a7d125125867832779a2ab72c1756a5ee5600
Fedomn <fedomn.ma@gmail.com>
2022-07-31 16:29:04 +0800

    refactor hash_agg fields and add detailed comments
    Signed-off-by: Fedomn <fedomn.ma@gmail.com>

--- src/executor/aggregate/hash_agg.rs
+++ src/executor/aggregate/hash_agg.rs
@@ -32,8 +32,7 @@ impl HashAggExecutor {
     pub async fn execute(self) {
         let agg_funcs = self.cast_agg_funcs();
 
-        let mut group_fileds: Option<Vec<Field>> = None;
-        let mut agg_fileds: Option<Vec<Field>> = None;
+        let mut group_and_agg_fields: Option<Vec<Field>> = None;
         let mut group_hashs = Vec::new();
         let mut group_hash_2_keys = HashMap::new();
         let mut group_hash_2_accs = HashMap::new();
@@ -41,47 +40,48 @@ impl HashAggExecutor {
         #[for_await]
         for batch in self.child {
             let batch = batch?;
-            // only support one epxrssion in aggregation, not supported example: `sum(distinct a)`
+
+            // 1. build group and agg schema for hash_agg RecordBatch schema.
+            if group_and_agg_fields.is_none() {
+                let group_fileds = self.group_by.iter().map(|key| key.eval_field(&batch));
+
+                let agg_fileds = agg_funcs.iter().map(|agg| {
+                    let inner_name = agg.exprs[0].eval_field(&batch).name().clone();
+                    let new_name = format!("{}({})", agg.func, inner_name);
+                    Field::new(new_name.as_str(), agg.return_type.clone(), false)
+                });
+
+                group_and_agg_fields = Some(
+                    group_fileds
+                        .into_iter()
+                        .chain(agg_fileds.into_iter())
+                        .collect::<Vec<_>>(),
+                );
+            }
+
+            // 2.1 evaluate agg exprs and collect the result columns for later accumulators.
             let columns: Vec<_> = agg_funcs
                 .iter()
                 .map(|agg| agg.exprs[0].eval_column(&batch))
                 .try_collect()?;
 
-            // build group schema for aggregation result
-            if group_fileds.is_none() {
-                group_fileds = Some(
-                    self.group_by
-                        .iter()
-                        .map(|key| key.eval_field(&batch))
-                        .collect(),
-                );
-            }
-            // build agg schema for aggregation result
-            if agg_fileds.is_none() {
-                agg_fileds = Some(
-                    agg_funcs
-                        .iter()
-                        .map(|agg| {
-                            let inner_name = agg.exprs[0].eval_field(&batch).name().clone();
-                            let new_name = format!("{}({})", agg.func, inner_name);
-                            Field::new(new_name.as_str(), agg.return_type.clone(), false)
-                        })
-                        .collect(),
-                );
-            }
-
-            // evaluate group by columns
+            // 2.2 evaluate group by exprs and collect group by columns for row hash calculation.
             let group_keys: Vec<_> = self
                 .group_by
                 .iter()
                 .map(|expr| expr.eval_column(&batch))
                 .try_collect()?;
 
-            // build group by columns hash key
+            // 3.1 build row hash key from group by columns.
             let mut every_rows_hashes = vec![0; batch.num_rows()];
             create_hashes(&group_keys, &Default::default(), &mut every_rows_hashes)?;
 
-            // build accumulator map, row indices map and group keys map
+            // 3.2
+            // a. build accumulator map(group_hash_2_accs) for aggregation calculation.
+            // b. build group row indices map(group_hash_2_row_indices) to take one group rows
+            // from a column, and use acc.update_batch to calculate the group result.
+            // c. build group values map(group_hash_2_keys) to record the group values.
+            // d. build group hashs vector(group_hashs) to record distinct group hash key order.
             let mut group_hash_2_row_indices = HashMap::new();
             for (row, hash) in every_rows_hashes.iter().enumerate() {
                 if !group_hash_2_accs.contains_key(hash) {
@@ -105,29 +105,26 @@ impl HashAggExecutor {
                     .append_value(row as u32)?;
             }
 
-            // finish aggregation result for each group
+            // 4. finish aggregation result for each group.
             for (hash, mut idx_builder) in group_hash_2_row_indices {
                 let indices = idx_builder.finish();
                 let accs = group_hash_2_accs.get_mut(&hash).unwrap();
                 for (acc, column) in accs.iter_mut().zip_eq(columns.iter()) {
+                    // take one group rows from a column
                     let new_array = compute::take(column.as_ref(), &indices, None)?;
                     acc.update_batch(&new_array)?;
                 }
             }
         }
 
-        // build result builders
-        let fields = group_fileds
-            .unwrap()
-            .into_iter()
-            .chain(agg_fileds.unwrap().into_iter())
-            .collect::<Vec<_>>();
+        // 5.1 build result builders for hash_agg RecordBatch.
+        let fields = group_and_agg_fields.unwrap();
         let mut builders = fields
             .iter()
             .map(|f| build_scalar_value_builder(f.data_type()))
             .collect::<Vec<_>>();
 
-        // convert row data to columnar data
+        // 5.2 convert row data to columnar data using builders.
         for hash in group_hashs {
             let group_values = group_hash_2_keys.get(&hash).unwrap();
             for (idx, group_key) in group_values.iter().enumerate() {
@@ -142,21 +139,8 @@ impl HashAggExecutor {
             }
         }
 
-        // for (hash, group_values) in group_hash_2_keys.iter() {
-        //     for (idx, group_key) in group_values.iter().enumerate() {
-        //         append_scalar_value_for_builder(group_key, &mut builders[idx])?;
-        //     }
-
-        //     for (idx, acc) in group_hash_2_accs.get(hash).unwrap().iter().enumerate() {
-        //         append_scalar_value_for_builder(
-        //             &acc.evaluate()?,
-        //             &mut builders[idx + group_values.len()],
-        //         )?;
-        //     }
-        // }
-
+        // 6. finish result builders and build RecordBatch.
         let columns = builders.iter_mut().map(|b| b.finish()).collect::<Vec<_>>();
-
         let schema = SchemaRef::new(Schema::new(fields));
         yield RecordBatch::try_new(schema, columns)?;
     }
