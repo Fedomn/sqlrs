975bd1b615addea6dde136d16e122f0b1d54bdcf
Fedomn <fedomn.ma@gmail.com>
2022-08-16 22:03:59 +0800

    extract main create_csv_table method
    Signed-off-by: Fedomn <fedomn.ma@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,21 @@ use sql_query_engine_rs::{cli, Database};
 #[tokio::main]
 async fn main() -> Result<()> {
     let db = Database::new_on_csv();
-    let table_name = "employee".to_string();
-    let filepath = "./tests/csv/employee.csv".to_string();
-    db.create_csv_table(table_name, filepath)?;
-    let table_name = "department".to_string();
-    let filepath = "./tests/csv/department.csv".to_string();
-    db.create_csv_table(table_name, filepath)?;
-    let table_name = "state".to_string();
-    let filepath = "./tests/csv/state.csv".to_string();
-    db.create_csv_table(table_name, filepath)?;
+    create_csv_table(&db, "employee")?;
+    create_csv_table(&db, "department")?;
+    create_csv_table(&db, "state")?;
+    create_csv_table(&db, "t1")?;
+    create_csv_table(&db, "t2")?;
 
     cli::interactive(db).await?;
+
+    Ok(())
+}
+
+fn create_csv_table(db: &Database, table_name: &str) -> Result<()> {
+    let table_name = table_name.to_string();
+    let filepath = format!("./tests/csv/{}.csv", table_name);
+    db.create_csv_table(table_name, filepath)?;
 
     Ok(())
 }
