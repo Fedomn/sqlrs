6169452bc8ae782f8b56d7950dbc3e5b08a9fdf1
Fedomn <fedomn.ma@gmail.com>
2022-12-21 11:31:58 +0800

    feat(planner): describe table info
    Signed-off-by: Fedomn <fedomn.ma@gmail.com>

---
+++ src/planner_v2/binder/statement/bind_explain_table.rs
@@ -0,0 +1,34 @@
+use sqlparser::ast::Statement;
+
+use super::BoundStatement;
+use crate::planner_v2::{
+    BindError, Binder, SqlparserQueryBuilder, SqlparserResolver, SqlparserSelectBuilder,
+};
+
+impl Binder {
+    pub fn bind_explain_table(&mut self, stmt: &Statement) -> Result<BoundStatement, BindError> {
+        match stmt {
+            Statement::ExplainTable {
+                describe_alias,
+                table_name,
+                ..
+            } => {
+                if !*describe_alias {
+                    return Err(BindError::UnsupportedStmt(
+                        "Only support describe table statement".to_string(),
+                    ));
+                }
+                let (_, _table_name) = SqlparserResolver::object_name_to_schema_table(table_name)?;
+                // FIXME: support filter table_name
+                let select = SqlparserSelectBuilder::default()
+                    .projection_wildcard()
+                    .from_table_function("sqlrs_columns")
+                    .build();
+                let query = SqlparserQueryBuilder::new_from_select(select).build();
+                let node = self.bind_select_node(&query)?;
+                self.create_plan_for_select_node(node)
+            }
+            _ => Err(BindError::UnsupportedStmt(format!("{:?}", stmt))),
+        }
+    }
+}
